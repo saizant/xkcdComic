7a740ce48b7219ad08f866cbcce72c8d24ea231f
saizant <saizant>
2017-01-11 21:55:36 +0100

    Ejercicio realizado

--- app/src/main/java/es/schooleando/xkcdcomic/ComicActivity.java
+++ app/src/main/java/es/schooleando/xkcdcomic/ComicActivity.java
@@ -1,19 +1,37 @@
 package es.schooleando.xkcdcomic;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ComicActivity extends AppCompatActivity implements BgResultReceiver.Receiver {
     private BgResultReceiver mResultReceiver;
+
+    //Vistas del layout
+    private ImageView imagen;
+    private ProgressBar progressBar;
+
+    private int topComic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic);
 
+        //Enganchar vistas del layout
+        imagen = (ImageView)findViewById(R.id.imageView);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        //Instanciar el BgResultReceiver
         mResultReceiver = new BgResultReceiver(new Handler());
         mResultReceiver.setReceiver(this);
 
@@ -22,6 +40,23 @@ public class ComicActivity extends AppCompatActivity implements BgResultReceiver
         intent.putExtra("url", "http://xkcd.com/info.0.json");
         intent.putExtra("receiver", mResultReceiver);
         startService(intent);
+
+        // TODO: Falta un callback de ImageView para hacer click en la imagen y que se descargue otro comic aleatorio.
+        //Listener al pinchar en la imagen
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int aleatorio = ThreadLocalRandom.current().nextInt(1, topComic+1);
+                    Intent intent = new Intent(ComicActivity.this, DownloadIntentService.class);
+                    intent.putExtra("url", "http://xkcd.com/" + aleatorio + "/info.0.json");
+                    intent.putExtra("receiver", mResultReceiver);
+                    startService(intent);
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(ComicActivity.this, "Excepción IllegalArgument al cambiar de imagen", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
@@ -33,8 +68,41 @@ public class ComicActivity extends AppCompatActivity implements BgResultReceiver
         //      OK -> nos hemos descargado la imagen correctamente. (ImageView)
         // Debeis controlar cada caso
 
+        //Si el Comic descargado es el último, se descargará el siguiente de forma aleatoria
+        if (resultData.getBoolean("esUltimoComic")) {
+            topComic = resultData.getInt("numComic");
+        }
+
+        //Según los resultados recibidos del IntentService
+        switch (resultCode) {
+            //      ERROR -> ha habido un problema de la conexión (Toast)
+            case DownloadIntentService.ERROR:
+                String mensaje = resultData.getString("mensaje");
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(ComicActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+                break;
+
+            //      PROGRESS -> nos estamos descargando la imagen (ProgressBar)
+            case DownloadIntentService.PROGRESS:
+                int progreso = resultData.getInt("progreso");
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setIndeterminate(progreso < 0);
+                progressBar.setProgress(progreso);
+                break;
+
+            //      OK -> nos hemos descargado la imagen correctamente. (ImageView)
+            case DownloadIntentService.OK:
+                String urlString = resultData.getString("urlString");
+                progressBar.setVisibility(View.INVISIBLE);
+                File fichero = new File(urlString);
+                if (fichero.exists()) {
+                    imagen.setImageBitmap(BitmapFactory.decodeFile(fichero.getAbsolutePath()));
+                } else {
+                    Toast.makeText(ComicActivity.this, "El fichero no existe", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
 
     }
 
-    // TODO: Falta un callback de ImageView para hacer click en la imagen y que se descargue otro comic aleatorio.
 }
--- app/src/main/java/es/schooleando/xkcdcomic/DownloadIntentService.java
+++ app/src/main/java/es/schooleando/xkcdcomic/DownloadIntentService.java
@@ -1,15 +1,38 @@
 package es.schooleando.xkcdcomic;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
 public class DownloadIntentService extends IntentService  {
     private static final String TAG = DownloadIntentService.class.getSimpleName();
     private ResultReceiver mReceiver;
+
+    //CONSTANTES para devolver resultados (resultCodes)
+    public static final int ERROR = 0;
+    public static final int PROGRESS = 1;
+    public static final int OK = 2;
 
     public DownloadIntentService() {
         super("DownloadIntentService");
@@ -20,17 +43,130 @@ public class DownloadIntentService extends IntentService  {
         mReceiver = intent.getParcelableExtra("receiver");
         Log.d(TAG, "onHandleIntent");
 
+        String urlString = intent.getStringExtra("url");
+        Bundle bundle = new Bundle();
+
+        //Informar de que el Comic es el actual (último publicado)
+        bundle.putBoolean("esUltimoComic", urlString.equals("http://xkcd.com/info.0.json"));
+
         // TODO Aquí hacemos la conexión y accedemos a la imagen.
         //
         // TODO: Habrá que hacer 2 conexiones:
         //  1. Para descargar el resultado JSON para leer la URL.
         //  2. Una vez tenemos la URL descargar la imagen en la carpeta temporal.
 
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
-        // TODO: Devolver la URI de la imagen si todo ha ido bien.
+        //Si está conectado...
+        if (networkInfo != null && networkInfo.isConnected()) {
+            HttpURLConnection conexion = null;
 
-        // TODO: Controlar los casos en los que no ha ido bien: excepciones en las conexiones, etc...
+            try {
+                //Conexión
+                URL url = new URL(urlString);
+                conexion = (HttpURLConnection)url.openConnection();
 
-        mReceiver.send(0, Bundle.EMPTY);  // cambiar
+                StringBuilder resultado = new StringBuilder();
+
+                //Flujos entrada
+                InputStream is = new BufferedInputStream(conexion.getInputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                //Se va leyendo y añadiendo
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    resultado.append(linea);
+                }
+
+                //Desconexión
+                conexion.disconnect();
+
+                //Descargar el resultado JSON para leer la URL y descargar la imagen en carpeta temporal
+                JSONObject jsonObject = new JSONObject(resultado.toString());
+                bundle.putInt("numComic", jsonObject.getInt("num"));
+                String urlComic = jsonObject.getString("img");
+
+                url = new URL(urlComic);
+                conexion = (HttpURLConnection)url.openConnection();
+                conexion.setConnectTimeout(7000);
+                conexion.setReadTimeout(7000);
+                conexion.setRequestMethod("HEAD");
+                conexion.connect();
+
+                //Comprobar respuesta del servidor
+                int codigoRespuesta = conexion.getResponseCode();
+                if (codigoRespuesta == 200) {
+                    int tamanyo = conexion.getContentLength();
+                    is = url.openStream();
+
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+
+                    //Escritura y progreso
+                    for (int i; (i = is.read(buffer)) != -1; ) {
+                        bos.write(buffer, 0, i);
+                        if (tamanyo > 0) {
+                            bundle.putInt("progreso", bos.size() * 100 / tamanyo);
+                        } else {
+                            bundle.putInt("progreso", i*-1);
+                        }
+                        mReceiver.send(PROGRESS, bundle);
+                    }
+
+                    //Temporal
+                    File outputDir = getExternalCacheDir();
+                    String[] datos = urlComic.split("/");
+                    String[] trozo = datos[datos.length-1].split("\\.");
+                    File outputFile = File.createTempFile(trozo[0], "." + trozo[1], outputDir);
+                    outputFile.deleteOnExit();      //Eliminar temporal al SALIR
+
+                    //Flujos salida
+                    FileOutputStream fos = new FileOutputStream(outputFile);
+                    fos.write(bos.toByteArray());
+
+                    // TODO: Devolver la URI de la imagen si todo ha ido bien.
+                    bundle.putString("urlString", outputFile.getPath());
+                    mReceiver.send(OK, bundle);
+
+                    //Cerrar flujos I/O
+                    bos.close();
+                    is.close();
+                    fos.close();
+
+                } else {
+                    bundle.putString("mensaje", "ERROR del código de respuesta del servidor: " + codigoRespuesta);
+                    mReceiver.send(ERROR, bundle);
+                }
+
+                // TODO: Controlar los casos en los que no ha ido bien: excepciones en las conexiones, etc...
+            } catch (MalformedURLException e) {
+                bundle.putString("mensaje", "URL incorrecta");
+                mReceiver.send(ERROR, bundle);
+            } catch (SocketTimeoutException e) {
+                bundle.putString("mensaje", "Excepción TimeOut");
+                mReceiver.send(ERROR, bundle);
+            } catch (IOException e) {
+                bundle.putString("mensaje", "Error de I/O");
+                mReceiver.send(ERROR, bundle);
+            } catch (JSONException e) {
+                bundle.putString("mensaje", "Error en el JSON");
+                mReceiver.send(ERROR, bundle);
+            } catch (Exception e) {
+                bundle.putString("mensaje", "Excepción: " + e.getMessage());
+                mReceiver.send(ERROR, bundle);
+            } finally {
+                //Se comprueba siempre si la conexión sigue para desconectarla
+                if (conexion != null) {
+                    conexion.disconnect();
+                }
+            }
+
+        } else {
+            bundle.putString("mensaje", "No está conectado");
+            mReceiver.send(ERROR, bundle);
+        }
+
+        //mReceiver.send(0, Bundle.EMPTY);  // cambiar
     }
 }
